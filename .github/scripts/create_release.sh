3d5b32f678e6728ea2958d08a7e35372d6dee137
Charlie Powell <cdp1337@veraciousnetwork.com>
2024-12-18 20:31:25 -0500

    Work on github workflow

--- .github/scripts/create_release.sh
+++ .github/scripts/create_release.sh
@@ -2,5 +2,29 @@
 #
 # Create a release on GitHub
 
-echo "test test"
-echo "$REF"
+if [ ! -e "RELEASE.md" ]; then
+	echo "ERROR - No RELEASE.md file located!"
+	echo "Please create a RELEASE.md file in the root of your repository"
+	echo "  which contains the release notes for this version."
+	exit 1
+fi
+
+if [ "$REF" == "" ]; then
+	echo "ERROR - No 'REF' environment variable set!"
+	echo "Example YAML step:"
+	echo "      - name: Create Release"
+	echo "        id: create_release"
+	echo "        env:"
+	echo "          REF: \${{ github.ref }}"
+	exit 1
+fi
+
+if [[ "$REF" =~ "refs/heads/" ]]; then
+	gh release create -d -p -F RELEASE.md
+elif [[ "$REF" =~ "refs/tags" ]]; then
+	gh release create -d -F RELEASE.md
+else
+	echo "ERROR - Invalid 'REF' environment variable value!"
+	echo "Expected 'REF' to be either 'refs/heads/...' or 'refs/tags/...'"
+	exit 1
+fi
